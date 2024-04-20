4405271f036d04fee339a87b1ebbfe4f9c53e522
Carlos Roldan <120054943+CR3op@users.noreply.github.com>
2024-04-20 02:36:52 +0000

    added the contents for the task1 and task2.py files.

--- task1.py
+++ task1.py
@@ -9,3 +9,9 @@ path = '/workspaces/NoTown/no_town.csv'
 csv_file = pd.read_csv(path)
 print(csv_file.to_string())
 
+#create database file called 'original_db.db'
+con = sqlite3.connect('original_db.db')
+
+#create a cursor to help create sql statements by creating the table and so on
+cursor = con.cursor()
+
---
+++ task2.py
@@ -0,0 +1,28 @@
+
+import pandas as pd
+import sqlite3
+import csv
+
+path = '/workspaces/NoTown/no_town.csv'
+
+csv_file = pd.read_csv(path)
+print(csv_file.to_string())
+
+
+#create database file called 'original_db.db'
+con = sqlite3.connect('original_db.db')
+
+#create a cursor to help create sql statements by creating the table and so on
+cursor = con.cursor()
+
+cursor.execute('''CREATE TABLE musician(
+                SSN INTEGER PRIMARY_KEY,
+                address TEXT,
+                name TEXT
+                )''')
+
+#commit changes
+con.commit()
+
+#close connection
+con.close()
